7cf3057867c2e121a2b8d76297346eb89f30439c
RasmusKEA <rasm937k@stud.kea.dk>
2022-12-17 21:54:17 +0100

    deleted comments

--- Test exam/Test exam/Models/Comment.cs
+++
@@ -1,9 +0,0 @@
-namespace Test_exam.Models;
-
-public class Comment
-{
-    public int Id { get; set; }
-    public int IdUser {get; set;}
-    public int idReview { get; set; }
-    public string UserComment { get; set; }
-}
